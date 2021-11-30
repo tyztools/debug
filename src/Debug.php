118247bec6747e21759da72c826606c04d53fbf9
tianyunchong <854024109a>
2021-11-30 16:52:04 +0800

    增加var_dump方法

--- src/Debug.php
+++ src/Debug.php
@@ -14,6 +14,20 @@ class Debug
         static::dispatch($info);
     }
 
+    public static function var_dump($arr, $project="") {
+        $debugRes = debug_backtrace();
+        if (!is_array($arr)) {
+            $arr = array($arr);
+        }
+        $info     = array(
+            "uuid"     => Identity::getUuid($project),
+            "data"     => var_export($arr, true),
+            "line"     => isset($debugRes[0]["line"]) ?  $debugRes[0]["line"] : 0,
+            "filepath" => isset($debugRes[0]["file"]) ? $debugRes[0]["file"] : "",
+        );
+        static::dispatch($info);
+    }
+
     public static function dispatch($data)
     {
         if (!extension_loaded("swoole")) {
@@ -23,7 +37,7 @@ class Debug
         if (!$client->connect(Receiver::$host, Receiver::$port, -1)) {
             return;
         }
-        $client->send(json_encode($data).Receiver::$seperator);
+        $client->send(json_encode($data, JSON_UNESCAPED_UNICODE).Receiver::$seperator);
         $client->close();
     }
 
